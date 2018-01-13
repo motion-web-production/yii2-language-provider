186049c8b86500a1b7eede242fd0c716ea439031
greeflas <vldmr.kuprienko@gmail.com>
2018-01-13 16:46:40 +0200

    Creates language helper

---
+++ src/helpers/LanguageHelper.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * @link https://github.com/motion/yii2-language-provider
+ * @copyright Copyright (c) 2017 Motion Web Production
+ * @license BSD 3-Clause License
+ */
+
+namespace motion\i18n\helpers;
+
+use Yii;
+use yii\helpers\ArrayHelper;
+use motion\i18n\LanguageProviderInterface;
+
+/**
+ * Helper for making work with language provider easily.
+ *
+ * @author Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
+ * @since 2.1
+ */
+class LanguageHelper
+{
+    /**
+     * @var LanguageProviderInterface
+     */
+    protected $provider;
+
+    /**
+     * @var LanguageHelper
+     */
+    private static $_instance;
+
+
+    /**
+     * Gets the instance via lazy initialization.
+     *
+     * @return LanguageHelper
+     *
+     * @throws \yii\base\InvalidConfigException
+     * @throws \yii\di\NotInstantiableException
+     */
+    public static function getInstance()
+    {
+        if (null === self::$_instance) {
+            self::$_instance = new self();
+            self::$_instance->provider = Yii::$container->get(LanguageProviderInterface::class);
+        }
+
+        return self::$_instance;
+    }
+
+    /**
+     * Returns list of languages.
+     *
+     * @return array
+     */
+    public function getAll()
+    {
+        return $this->provider->getLanguages();
+    }
+
+    /**
+     * Returns default application language.
+     *
+     * @return array
+     */
+    public function getDefault()
+    {
+        return $this->provider->getDefaultLanguage();
+    }
+
+    /**
+     * Returns language label.
+     *
+     * @param null|string $locale By default uses current application language.
+     *
+     * @return string
+     */
+    public function getLabel($locale = null)
+    {
+        $locale = null === $locale ? Yii::$app->language : $locale;
+
+        return $this->provider->getLanguageLabel($locale);
+    }
+
+    /**
+     * Returns language locales.
+     *
+     * @return array
+     */
+    public function getLocales()
+    {
+        return ArrayHelper::getColumn($this->provider->getLanguages(), 'locale');
+    }
+
+    /**
+     * Is not allowed to call from outside to prevent from creating multiple instances,
+     * to use the singleton, you have to obtain the instance from getInstance() instead.
+     */
+    private function __construct()
+    {
+    }
+
+    /**
+     * Prevent the instance from being cloned (which would create a second instance of it).
+     */
+    private function __clone()
+    {
+    }
+
+    /**
+     * Prevent from being unserialized (which would create a second instance of it).
+     */
+    private function __wakeup()
+    {
+    }
+}
---
+++ tests/unit/helpers/LanguageHelperTest.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * @link https://github.com/motion/yii2-language-provider
+ * @copyright Copyright (c) 2017 Motion Web Production
+ * @license BSD 3-Clause License
+ */
+
+namespace motion\i18n\tests\unit\helpers;
+
+use Yii;
+use motion\i18n\tests\unit\TestCase;
+use motion\i18n\LanguageProviderInterface;
+use motion\i18n\ConfigLanguageProvider;
+use motion\i18n\helpers\LanguageHelper;
+
+/**
+ * Test case for language helper.
+ *
+ * @author Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
+ * @since 2.1
+ */
+class LanguageHelperTest extends TestCase
+{
+    /**
+     * @var array
+     */
+    private $languages = [
+        ['locale' => 'en', 'label' => 'English'],
+        ['locale' => 'uk', 'label' => 'Ukrainian'],
+        ['locale' => 'fr', 'label' => 'French'],
+    ];
+    /**
+     * @var array
+     */
+    private $defaultLanguage = ['locale' => 'en', 'label' => 'English'];
+
+
+    /**
+     * @inheritdoc
+     */
+    protected function _before()
+    {
+        Yii::$container->set(LanguageProviderInterface::class, [
+            'class' => ConfigLanguageProvider::class,
+            'languages' => $this->languages,
+            'defaultLanguage' => $this->defaultLanguage,
+        ]);
+    }
+
+    public function testGetAll()
+    {
+        $this->assertSame($this->languages, LanguageHelper::getInstance()->getAll());
+    }
+
+    public function testGetDefault()
+    {
+        $this->assertSame($this->defaultLanguage, LanguageHelper::getInstance()->getDefault());
+    }
+
+    public function testGetLabel()
+    {
+        $this->assertSame('French', LanguageHelper::getInstance()->getLabel('fr'));
+        $this->assertSame('Ukrainian', LanguageHelper::getInstance()->getLabel('uk'));
+        $this->assertSame('English', LanguageHelper::getInstance()->getLabel('en'));
+    }
+
+    public function testGetLocales()
+    {
+        $this->assertSame(array_column($this->languages, 'locale'), LanguageHelper::getInstance()->getLocales());
+    }
+}
